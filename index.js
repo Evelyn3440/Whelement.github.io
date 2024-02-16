e5004f6b31877bef9a02c288c767d5824fcc62ca
Evelyn Long <brocklong344@gmail.com>
2024-02-15 20:30:47 -0500

    meow meow mrrp mrrp

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express()
 const path = require('path');
 const port = 3000
-
+var privateKey = fs.readFileSync( 'private_key.cem' );
+var certificate = fs.readFileSync( 'certif.cem' );
 // sendFile will go here
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
 app.use(express.static('public'))
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+https.createServer({
+    key: privateKey,
+    cert: certificate
+}, app).listen(port);
