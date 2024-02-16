914251dedcab7dab5f0f45a786209bcada8ceb7d
Evelyn Long <brocklong344@gmail.com>
2024-02-16 21:08:22 +0000

    Trolling :3

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const express = require('express')
 const app = express()
 const path = require('path');
-const port = 3000
-var privateKey = fs.readFileSync( 'private_key.cem' );
-var certificate = fs.readFileSync( 'certif.cem' );
+const port = 8000
 // sendFile will go here
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
 app.use(express.static('public'))
-https.createServer({
-    key: privateKey,
-    cert: certificate
-}, app).listen(port);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
