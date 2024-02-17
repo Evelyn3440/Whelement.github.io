2e0454fb81e3a7dfa852abec5650368c40eea704
Evelyn3440 <82115237+Evelyn3440@users.noreply.github.com>
2024-02-17 00:40:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const port = 8000
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
+// sendFile will go here
+app.get('/crsh1tty', function(req, res) {
+  res.sendFile(path.join(__dirname, '/crsh1tty/index.html'));
+});
 app.use(express.static('public'))
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
