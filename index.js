7087b74546f1ebe16440ccfcf253e6cf8ca7f2e9
Evelyn Long <brocklong344@gmail.com>
2024-02-18 23:31:46 +0000

    mrow meow meow rrp

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const express = require('express')
 const app = express()
 const path = require('path');
 const port = 8000
-// sendFile will go here
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
-// sendFile will go here
 app.get('/crsh1tty', function(req, res) {
   res.sendFile(path.join(__dirname, '/crsh1tty/index.html'));
 });
+app.get('/favicon.svg',function(req, res) {
+  console.log("You good bro?");
+  res.sendFile(path.join(__dirname, 'whelement.svg'));
+})
 app.use(express.static('public'))
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
